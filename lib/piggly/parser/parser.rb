kputnam/piggly

commit 06bdb0270162945345f7f3c68ee5212b21c8baf5

Fixed coverage tag generation in "FOR var IN n .. m". Also fixed incorrect parse of procedure return type

lib/piggly/parser/grammar.tt
@@ -13,6 +13,7 @@ grammar Piggly
     procedureHeader
     name:tIdentifier tSpace?
     parameters:parameterList tSpace
+    procedureReturn tSpace
     return:tType tSpace kwAS tSpace
     procedureBody
     procedureFooter <Procedure>
@@ -23,7 +24,7 @@ grammar Piggly
   end
 
   rule procedureReturn
-    'returns' tSpace ( 'set' tSpace 'of' tSpace )? <TextNode>
+    'returns' tSpace 'setof'? <TextNode>
   end
 
   rule procedureBody
@@ -176,8 +177,8 @@ grammar Piggly
     tSpace? ';' <WhileLoop>
   end
   
+  # loop coverage calculated by using exitStub
   rule stmtForLoop
-    # loop coverage calculated by using exitStub
     label_open:tLabel tSpace
     kwFOR tSpace identifierList tSpace
     kwIN
@@ -254,7 +255,6 @@ grammar Piggly
     expr:expressionUntilSemiColon ';' <Raise>
   end
   
-  # todo stmt_execsql
   rule stmtExecSql
     sqlKeyword tSpace expressionUntilSemiColon ';' <Sql>
   end
@@ -428,6 +428,7 @@ grammar Piggly
     "'"  content:("''" / [^'])* "'" <TString>
   end
   
+  # beware this is quite broad and might match things it shouldn't
   rule tType
     [a-z\200-\377_]
     ( '(' rType ')' /
@@ -744,13 +745,3 @@ grammar Piggly
   end
   
 end
-
-# expr   -> term   '+' expr | term
-# term   -> factor '*' term | factor
-# factor -> digit | '(' expr ')'
-# digit  -> '0' | '1' | ...
-# 
-# expr   -> term   ( '+' expr | '' )
-# term   -> factor ( '*' term | '' )
-# factor -> digit | '(' expr ')'
-# digit  -> '0' | '1' | ...

lib/piggly/parser/parser.rb
@@ -57,7 +57,7 @@ module Piggly
       elements[4]
     end
 
-    def return
+    def procedureReturn
       elements[5]
     end
 
@@ -65,7 +65,7 @@ module Piggly
       elements[6]
     end
 
-    def kwAS
+    def return
       elements[7]
     end
 
@@ -73,12 +73,20 @@ module Piggly
       elements[8]
     end
 
-    def procedureBody
+    def kwAS
       elements[9]
     end
 
-    def procedureFooter
+    def tSpace4
       elements[10]
+    end
+
+    def procedureBody
+      elements[11]
+    end
+
+    def procedureFooter
+      elements[12]
     end
   end
 
@@ -114,23 +122,31 @@ module Piggly
             r6 = _nt_tSpace
             s0 << r6
             if r6
-              r7 = _nt_tType
+              r7 = _nt_procedureReturn
               s0 << r7
               if r7
                 r8 = _nt_tSpace
                 s0 << r8
                 if r8
-                  r9 = _nt_kwAS
+                  r9 = _nt_tType
                   s0 << r9
                   if r9
                     r10 = _nt_tSpace
                     s0 << r10
                     if r10
-                      r11 = _nt_procedureBody
+                      r11 = _nt_kwAS
                       s0 << r11
                       if r11
-                        r12 = _nt_procedureFooter
+                        r12 = _nt_tSpace
                         s0 << r12
+                        if r12
+                          r13 = _nt_procedureBody
+                          s0 << r13
+                          if r13
+                            r14 = _nt_procedureFooter
+                            s0 << r14
+                          end
+                        end
                       end
                     end
                   end
@@ -261,16 +277,6 @@ module Piggly
   end
 
   module ProcedureReturn0
-    def tSpace1
-      elements[1]
-    end
-
-    def tSpace2
-      elements[3]
-    end
-  end
-
-  module ProcedureReturn1
     def tSpace
       elements[1]
     end
@@ -301,38 +307,11 @@ module Piggly
       r2 = _nt_tSpace
       s0 << r2
       if r2
-        i4, s4 = index, []
-        if has_terminal?('set', false, index)
-          r5 = instantiate_node(SyntaxNode,input, index...(index + 3))
-          @index += 3
+        if has_terminal?('setof', false, index)
+          r4 = instantiate_node(SyntaxNode,input, index...(index + 5))
+          @index += 5
         else
-          terminal_parse_failure('set')
-          r5 = nil
-        end
-        s4 << r5
-        if r5
-          r6 = _nt_tSpace
-          s4 << r6
-          if r6
-            if has_terminal?('of', false, index)
-              r7 = instantiate_node(SyntaxNode,input, index...(index + 2))
-              @index += 2
-            else
-              terminal_parse_failure('of')
-              r7 = nil
-            end
-            s4 << r7
-            if r7
-              r8 = _nt_tSpace
-              s4 << r8
-            end
-          end
-        end
-        if s4.last
-          r4 = instantiate_node(SyntaxNode,input, i4...index, s4)
-          r4.extend(ProcedureReturn0)
-        else
-          @index = i4
+          terminal_parse_failure('setof')
           r4 = nil
         end
         if r4
@@ -345,7 +324,7 @@ module Piggly
     end
     if s0.last
       r0 = instantiate_node(TextNode,input, i0...index, s0)
-      r0.extend(ProcedureReturn1)
+      r0.extend(ProcedureReturn0)
     else
       @index = i0
       r0 = nil
@@ -11802,13 +11781,3 @@ class PigglyParser < Treetop::Runtime::CompiledParser
   include Piggly
 end
 
-
-# expr   -> term   '+' expr | term
-# term   -> factor '*' term | factor
-# factor -> digit | '(' expr ')'
-# digit  -> '0' | '1' | ...
-# 
-# expr   -> term   ( '+' expr | '' )
-# term   -> factor ( '*' term | '' )
-# factor -> digit | '(' expr ')'
-# digit  -> '0' | '1' | ...

lib/piggly/parser/nodes.rb
@@ -96,8 +96,8 @@ module Piggly
   class Procedure < NodeClass
   end
 
+  # ...;
   class Statement < NodeClass
-    # ...;
   end
 
   class Expression < NodeClass
@@ -108,7 +108,11 @@ module Piggly
     def tag(prefix = nil, id = nil)
       unless defined? @tag_id
         if named?(:cond)
-          if parent.loop?
+          if parent.for?
+            # this object is the conditional statement in a FOR loop
+            Piggly::ForCollectionTag.new(prefix, id)
+          elsif parent.loop?
+            # this object is the conditional statement in a WHILE loop
             Piggly::LoopConditionTag.new(prefix, id)
           elsif parent.branch?
             Piggly::BranchConditionTag.new(prefix, id)
@@ -120,111 +124,101 @@ module Piggly
     end
   end
 
+  # DECLARE declaration BEGIN body END;
   class Block < Statement
-    # DECLARE declaration BEGIN body END;
     def block?
       true
     end
   end
 
-
-
-  # branches with child 'cond' (Expression) will get a BranchCondTag
+  # Branches with child 'cond' (Expression) will get a BranchCondTag
   class Branch < Statement
     def branch?
       true
     end
   end
 
+  # IF boolean-cond THEN body
   class If < Branch
-    # IF boolean-cond THEN body
   end
 
+  # ELSE body END
   class Else < NodeClass
-    # ELSE body END
   end
 
+  # EXCEPTION WHEN boolean-cond THEN body
   class Catch < Branch
-    # EXCEPTION WHEN boolean-cond THEN body
   end
 
+  # WHEN match-expr THEN body
   class CaseWhen < Branch
-    # WHEN match-expr THEN body
   end
 
+  # WHEN boolean-cond THEN body
   class CondWhen < Branch
-    # WHEN boolean-cond THEN body
   end
 
+  # CONTINUE label WHEN boolean-cond;
   class ContinueWhen < Branch
-    # CONTINUE label WHEN boolean-cond;
   end
 
+  # EXIT label WHEN boolean-cond;
   class ExitWhen < Branch
-    # EXIT label WHEN boolean-cond;
   end
-
-
 
   class UnconditionalBranch < Statement
   end
 
-  # unconditional branches
+  # RETURN expr
   class Return < UnconditionalBranch
-    # RETURN expr
   end
 
+  # EXIT label
   class Exit < UnconditionalBranch
-    # EXIT label
   end
 
+  # CONTINUE label
   class Continue < UnconditionalBranch
-    # CONTINUE label
   end
 
+  # RAISE EXCEPTION expr
   class Throw < UnconditionalBranch
-    # RAISE EXCEPTION expr
   end
 
-
-
-  # loops with child 'cond' (Expression/Sql) will get a LoopCondTag
+  # Loops with child 'cond' (Expression/Sql) will get a LoopCondTag
   class Loop < Statement
     def loop?
       true
     end
   end
 
+  # FOR boolean-cond LOOP body END
   class ForLoop < Loop
-    # FOR boolean-cond LOOP body END
-    def for?
-      true
-    end
+    def for?; true end
   end
 
+  # WHILE boolean-cond LOOP body END
   class WhileLoop < Loop
-    # WHILE boolean-cond LOOP body END
   end
 
 
-
+  # RAISE NOTICE expr
   class Raise < Statement
-    # RAISE NOTICE expr
   end
 
+  # CASE search-expr WHEN ...
   class Case < Statement
-    # CASE search-expr WHEN ...
   end
 
+  # CASE WHEN ...
   class Cond < Statement
-    # CASE WHEN ...
   end
 
+  # lval := rval
   class Assignment < Statement
-    # lval := rval
   end
 
-  # lval of assignment (rval is an Expression)
+  # Lval of assignment (rval is an Expression)
   class Assignable < NodeClass
   end
 
@@ -243,9 +237,7 @@ module Piggly
     end
   end
 
-
-
-  # tokens have no children
+  # Tokens have no children
   class Token < NodeClass
     def initialize(input, interval, elements = nil)
       # prevent children from being assigned
@@ -257,8 +249,9 @@ module Piggly
     end
   end
 
+  # This seems like it should be a Token, but it may contain TComment children
+  # that should be highlighted differently than the enclosing whitespace
   class TWhitespace < NodeClass
-    # this seems like it should be a Token but it may contain TComment children
   end
 
   class TKeyword < Token
@@ -289,9 +282,7 @@ module Piggly
     def style; 'tL'; end
   end
 
-
-
-  # text nodes have no children
+  # Text nodes have no children
   class TextNode < NodeClass
     def initialize(input, interval, elements = nil)
       # prevent children from being assigned
@@ -303,6 +294,7 @@ module Piggly
     end
   end
   
+  # Stub nodes have no children, or content
   class StubNode < NodeClass
     def initialize(input, interval, elements = nil)
       # prevent children from being assigned
@@ -319,8 +311,8 @@ module Piggly
   end
 
   class NotImplemented < NodeClass
-    # this would go in the constructor, but parent is set from outside
     def parent=(object)
+      # this would go in the constructor, but parent is set from outside
       raise Piggly::Parser::Failure, "Grammar does not implement #{object.source_text} at line #{input.line_of(object.interval.first)}"
     end
   end

commit d9c76fa58b7f62364e33c64dc83bc460f3c0f90e

Fix parse failure for "returns setof ...", carelessly introduced by 06bdb0270162945345f7f3c68ee5212b21c8baf5

lib/piggly/parser/grammar.tt
@@ -13,7 +13,7 @@ grammar Piggly
     procedureHeader
     name:tIdentifier tSpace?
     parameters:parameterList tSpace
-    procedureReturn tSpace
+    procedureReturn
     return:tType tSpace kwAS tSpace
     procedureBody
     procedureFooter <Procedure>
@@ -24,7 +24,7 @@ grammar Piggly
   end
 
   rule procedureReturn
-    'returns' tSpace 'setof'? <TextNode>
+    'returns' tSpace ( 'setof' tSpace )? <TextNode>
   end
 
   rule procedureBody

lib/piggly/parser/parser.rb
@@ -61,32 +61,28 @@ module Piggly
       elements[5]
     end
 
-    def tSpace2
+    def return
       elements[6]
     end
 
-    def return
+    def tSpace2
       elements[7]
     end
 
-    def tSpace3
+    def kwAS
       elements[8]
     end
 
-    def kwAS
+    def tSpace3
       elements[9]
     end
 
-    def tSpace4
+    def procedureBody
       elements[10]
     end
 
-    def procedureBody
-      elements[11]
-    end
-
     def procedureFooter
-      elements[12]
+      elements[11]
     end
   end
 
@@ -125,27 +121,23 @@ module Piggly
               r7 = _nt_procedureReturn
               s0 << r7
               if r7
-                r8 = _nt_tSpace
+                r8 = _nt_tType
                 s0 << r8
                 if r8
-                  r9 = _nt_tType
+                  r9 = _nt_tSpace
                   s0 << r9
                   if r9
-                    r10 = _nt_tSpace
+                    r10 = _nt_kwAS
                     s0 << r10
                     if r10
-                      r11 = _nt_kwAS
+                      r11 = _nt_tSpace
                       s0 << r11
                       if r11
-                        r12 = _nt_tSpace
+                        r12 = _nt_procedureBody
                         s0 << r12
                         if r12
-                          r13 = _nt_procedureBody
+                          r13 = _nt_procedureFooter
                           s0 << r13
-                          if r13
-                            r14 = _nt_procedureFooter
-                            s0 << r14
-                          end
                         end
                       end
                     end
@@ -280,6 +272,12 @@ module Piggly
     def tSpace
       elements[1]
     end
+  end
+
+  module ProcedureReturn1
+    def tSpace
+      elements[1]
+    end
 
   end
 
@@ -307,11 +305,24 @@ module Piggly
       r2 = _nt_tSpace
       s0 << r2
       if r2
+        i4, s4 = index, []
         if has_terminal?('setof', false, index)
-          r4 = instantiate_node(SyntaxNode,input, index...(index + 5))
+          r5 = instantiate_node(SyntaxNode,input, index...(index + 5))
           @index += 5
         else
           terminal_parse_failure('setof')
+          r5 = nil
+        end
+        s4 << r5
+        if r5
+          r6 = _nt_tSpace
+          s4 << r6
+        end
+        if s4.last
+          r4 = instantiate_node(SyntaxNode,input, i4...index, s4)
+          r4.extend(ProcedureReturn0)
+        else
+          @index = i4
           r4 = nil
         end
         if r4
@@ -324,7 +335,7 @@ module Piggly
     end
     if s0.last
       r0 = instantiate_node(TextNode,input, i0...index, s0)
-      r0.extend(ProcedureReturn0)
+      r0.extend(ProcedureReturn1)
     else
       @index = i0
       r0 = nil