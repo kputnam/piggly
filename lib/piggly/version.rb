2b12d0a64fdf5432b60ed27c6129b983e6898a27
Kvle Putnam <putnam.kvle@gmail.com>
2010-04-22 23:15:07 +0000

    Bumped version for 1.2.1 release

--- lib/piggly/version.rb
+++ lib/piggly/version.rb
@@ -2,11 +2,11 @@ module Piggly
   module VERSION
     MAJOR = 1
     MINOR = 2
-    TINY  = 0
+    TINY  = 1
 
     STRING = [MAJOR, MINOR, TINY].join('.')
 
-    RELEASE_DATE = '2010-04-19'
+    RELEASE_DATE = '2010-04-22'
 
     def self.to_s
       STRING
