75f4ab9d9769ccdbbbf8e4e071c8aa0e1cd11638
Kvle Putnam <putnam.kvle@gmail.com>
2010-04-26 19:14:03 +0000

    Renamed TraceCompiler to Compiler::Trace and ReportCompiler to Compiler::Report

--- lib/piggly/compiler/report.rb
+++ lib/piggly/compiler/report.rb
@@ -1,66 +1,69 @@
 require File.join(File.dirname(__FILE__), *%w(.. reporter))
 
 module Piggly
+  module Compiler
 
-  #
-  # Produces HTML output to report coverage of tagged nodes in the tree
-  #
-  class ReportCompiler
-    include HtmlTag
+    #
+    # Produces HTML output to report coverage of tagged nodes in the tree
+    #
+    class Report
+      include HtmlTag
 
-    def self.compile(path, profile)
-      new(profile).send(:compile, path)
-    end
-
-    def initialize(profile)
-      @profile = profile
-    end
-
-  private
-
-    def compile(path)
-      lines = File.read(path).count("\n") + 1
-
-      # recompile (should be cache hit) to identify tagged nodes
-      data = TraceCompiler.cache(path)
-      html = traverse(data['tree'])
-
-      return 'html'  => html,
-             'lines' => 1..lines,
-             'tags'  => data['tags']
-    end
-
-    def traverse(node, string='')
-      if node.terminal?
-        # terminals (leaves) are never tagged
-        if node.style
-          string << '<span class="' << node.style << '">' << e(node.text_value) << '</span>'
-        else
-          string << e(node.text_value)
-        end
-      else
-        # non-terminals never write their text_value
-        node.elements.each do |child|
-          if child.tagged?
-
-            # retreive the profiled tag
-            tag = @profile.by_id[child.tag_id]
-
-            if tag.complete?
-              string << '<span class="' << tag.style << '" id="T' << tag.id << '">'
-            else
-              string << '<span class="' << tag.style << '" id="T' << tag.id << '" title="' << tag.description << '">'
-            end
-
-            traverse(child, string)
-            string << '</span>'
-          else
-            traverse(child, string)
-          end
-        end
+      def self.compile(path, profile)
+        new(profile).send(:compile, path)
       end
 
-      string
+      def initialize(profile)
+        @profile = profile
+      end
+
+    private
+
+      def compile(path)
+        lines = File.read(path).count("\n") + 1
+
+        # recompile (should be cache hit) to identify tagged nodes
+        data = Compiler::Trace.cache(path)
+        html = traverse(data['tree'])
+
+        return 'html'  => html,
+               'lines' => 1..lines,
+               'tags'  => data['tags']
+      end
+
+      def traverse(node, string='')
+        if node.terminal?
+          # terminals (leaves) are never tagged
+          if node.style
+            string << '<span class="' << node.style << '">' << e(node.text_value) << '</span>'
+          else
+            string << e(node.text_value)
+          end
+        else
+          # non-terminals never write their text_value
+          node.elements.each do |child|
+            if child.tagged?
+
+              # retreive the profiled tag
+              tag = @profile.by_id[child.tag_id]
+
+              if tag.complete?
+                string << '<span class="' << tag.style << '" id="T' << tag.id << '">'
+              else
+                string << '<span class="' << tag.style << '" id="T' << tag.id << '" title="' << tag.description << '">'
+              end
+
+              traverse(child, string)
+              string << '</span>'
+            else
+              traverse(child, string)
+            end
+          end
+        end
+
+        string
+      end
+
     end
 
   end
