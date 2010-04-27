7a17e010c0c1e8c9428e9631ccad63c9e232f2cd
Kvle Putnam <putnam.kvle@gmail.com>
2010-04-27 19:48:48 +0000

    Fixed require path for blankslate

--- lib/piggly/util.rb
+++ lib/piggly/util.rb
@@ -1,4 +1,4 @@
 require File.join(File.dirname(__FILE__), *%w[util enumerable])
 require File.join(File.dirname(__FILE__), *%w[util process_queue])
-require File.join(File.dirname(__FILE__), *%w[util blank_slate])
+require File.join(File.dirname(__FILE__), *%w[util blankslate])
 require File.join(File.dirname(__FILE__), *%w[util thunk])
