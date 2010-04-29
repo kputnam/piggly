b11651af58b8bbca4508ea4e528a57982f57b4f1
kputnam <putnam.kyle@gmail.com>
2010-04-29 20:51:59 +0000

    Reduced unsightly indentation by one level

--- lib/piggly/reporter/html/index.rb
+++ lib/piggly/reporter/html/index.rb
@@ -5,25 +5,26 @@ module Piggly
         class << self
 
           def output(procedures)
-            File.open(File.join(report_path, 'index.html'), 'w') do |io|
-              html(io) do
+            io = File.open(File.join(report_path, 'index.html'), 'w')
 
-                tag :html do
-                  tag :head do
-                    tag :title, 'Piggly PL/pgSQL Code Coverage'
-                    tag :link, :rel => 'stylesheet', :type => 'text/css', :href => 'piggly.css'
-                    tag :script, '<!-- -->', :type => 'text/javascript', :src => 'sortable.js'
-                  end
-
-                  tag :body do
-                    aggregate('PL/pgSQL Coverage Summary', Piggly::Profile.instance.summary)
-                    table(procedures.sort_by(&:name))
-                    timestamp
-                  end
+            html(io) do
+              tag :html do
+                tag :head do
+                  tag :title, 'Piggly PL/pgSQL Code Coverage'
+                  tag :link, :rel => 'stylesheet', :type => 'text/css', :href => 'piggly.css'
+                  tag :script, '<!-- -->', :type => 'text/javascript', :src => 'sortable.js'
                 end
 
+                tag :body do
+                  aggregate('PL/pgSQL Coverage Summary', Piggly::Profile.instance.summary)
+                  table(procedures.sort_by(&:name))
+                  timestamp
+                end
               end
             end
+
+          ensure
+            io.close
           end
 
         end
