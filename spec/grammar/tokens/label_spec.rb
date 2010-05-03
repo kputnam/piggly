c9ca98eba9539bcb70c0bd1285d5bb31529a8685
kputnam <putnam.kyle@gmail.com>
2010-05-03 14:59:23 +0000

    Recognize label targets, eg CONTINUE label

--- spec/grammar/tokens/label_spec.rb
+++ spec/grammar/tokens/label_spec.rb
@@ -7,31 +7,31 @@ module Piggly
     describe "labels" do
       it "must be enclosed in << and >>" do
         ['', 'a', 'abc', '<< a', 'a >>'].test_each do |s|
-          lambda{ parse(:tLabel, s) }.should raise_error
+          lambda{ parse(:tLabelDefinition, s) }.should raise_error
         end
       end
 
       it "can have space padding" do
         %w[a abc _].map{|s| "<< #{s} >>" }.test_each do |s|
-          parse(:tLabel, s).should be_a(Parser::Nodes::TLabel)
+          parse(:tLabelDefinition, s).should be_a(Parser::Nodes::TLabel)
         end
       end
 
       it "can have no space padding" do
         %w[a abc _].map{|s| "<<#{s}>>" }.test_each do |s|
-          parse(:tLabel, s).should be_a(Parser::Nodes::TLabel)
+          parse(:tLabelDefinition, s).should be_a(Parser::Nodes::TLabel)
         end
       end
 
       it "cannot be multiple unquoted words" do
         ["<< a b >>", "<< ab cd >>"].test_each do |s|
-          lambda{ parse(:tLabel, s) }.should raise_error
+          lambda{ parse(:tLabelDefinition, s) }.should raise_error
         end
       end
 
       it "can be enclosed in double quotes" do
         ['<< "a" >>', '<< "a b" >>', '<< "ab cd" >>'].test_each do |s|
-          parse(:tLabel, s).should be_a(Parser::Nodes::TLabel)
+          parse(:tLabelDefinition, s).should be_a(Parser::Nodes::TLabel)
         end
       end
     end
