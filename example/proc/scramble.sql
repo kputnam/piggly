b9f859ab0114b5eb83a754c84cd1f61e3f4e441f
Kvle Putnam <putnam.kvle@gmail.com>
2010-05-03 20:35:23 +0000

    Adding more examples

---
+++ example/proc/scramble.sql
@@ -0,0 +1,14 @@
+create or replace function scramble(subject text, out scrambled text, out first char)
+  as
+$$
+begin
+  scrambled := substring($1, 1, (random() * length($1))::integer);
+  first     := substring($1, 1, 1);
+end
+$$ language 'plpgsql' stable;
+
+create or replace function scramble(varchar, out text, out char) as $$
+  begin
+    execute scramble($1::text, $2, $3);
+  end
+$$ language 'plpgsql' stable;
