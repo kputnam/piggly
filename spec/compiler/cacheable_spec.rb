635142f3e51d0bf36ad6a4f2720f18ae8291a213
kputnam <putnam.kyle@gmail.com>
2010-05-11 21:59:05 -0500

    Wrote specs for Compiler::Cacheable

--- spec/compiler/cacheable_spec.rb
+++ spec/compiler/cacheable_spec.rb
@@ -3,31 +3,125 @@ require 'spec_helper'
 module Piggly
 
   describe Compiler::Cacheable do
+    before do
+      @compiler = Class.new { include Piggly::Compiler::Cacheable }
+      @compiler.stub(:name).and_return('TestCompiler')
+    end
 
     describe "stale?" do
+      it "compares cache_path with source path and cache_sources" do
+        @compiler.stub(:cache_sources).
+          and_return(%w(parser.rb grammar.tt nodes.rb))
+
+        @compiler.should_receive(:cache_path).
+          with('source.sql').
+          and_return('source.cache')
+
+        File.should_receive(:stale?).
+          with('source.cache', 'source.sql', 'parser.rb', 'grammar.tt', 'nodes.rb')
+
+        @compiler.stale?('source.sql')
+      end
     end
 
     describe "cache" do
-      context "when cache doesn't exist" do
-        it "parses the procedure source"
-        it "passes the parse tree and transient arguments to the compiler"
-        it "updates the cache with the results from the compiler"
-        it "returns the cache object"
+      before do
+        @procedure = mock('procedure')
+        @procedure.stub(:source_path).and_return('source path')
+        @procedure.stub(:source).and_return('SOURCE CODE')
+        @procedure.stub(:name).and_return('f')
       end
 
       context "when cache is stale" do
-        it "parses the procedure source"
-        it "passes the parse tree and transient arguments to the compiler"
-        it "updates the cache with the results from the compiler"
-        it "returns the cache object"
+        before do
+          @compiler.should_receive(:stale?).
+            and_return(true)
+
+          File.should_receive(:read).
+            with(@procedure.source_path).
+            and_return(@procedure.source)
+        end
+
+        it "parses the procedure source" do
+          @compiler.stub(:compile).
+            and_return(mock('result', :null_object => true))
+          Compiler::Cacheable::CacheDirectory.stub(:lookup).
+            and_return(mock('cache', :null_object => true))
+
+          Parser.should_receive(:parse).
+            with(@procedure.source)
+
+          @compiler.cache(@procedure)
+        end
+
+        it "passes the parse tree and transient arguments to the compiler" do
+          tree  = mock('parse tree', :null_object => true)
+          args  = %w(a b c)
+          block = lambda{|a,b| b }
+
+          Parser.stub(:parse).and_return(tree)
+          Compiler::Cacheable::CacheDirectory.stub(:lookup).
+            and_return(mock('cache', :null_object => true))
+
+          # calling cache method below should pass the parse tree plus any
+          # arguments given to cache along to the abstract 'compile' method
+          @compiler.should_receive(:compile).
+            with(tree, *args.push(block)).
+            and_return(mock('result', :null_object => true))
+
+          @compiler.cache(@procedure, *args, &block)
+        end
+
+        it "updates the cache with the results from the compiler" do
+          cache  = mock('cache')
+          result = mock('result')
+
+          Parser.stub(:parse).
+            and_return(mock('parse tree', :null_object => true))
+          @compiler.should_receive(:compile).
+            # with parse tree
+            and_return(result)
+
+          Compiler::Cacheable::CacheDirectory.should_receive(:lookup).
+            and_return(cache)
+          cache.should_receive(:replace).
+            with(result)
+
+          @compiler.cache(@procedure)
+        end
+
+        it "returns the cache object" do
+          Parser.stub(:parse).
+            and_return(mock('parse tree', :null_object => true))
+          @compiler.should_receive(:compile).
+            and_return(mock('result'))
+
+          cache = mock('cache')
+          cache.stub(:replace)
+
+          Compiler::Cacheable::CacheDirectory.should_receive(:lookup).
+            and_return(cache)
+          
+          @compiler.cache(@procedure).should == cache
+        end
       end
 
       context "when cache is fresh" do
-        it "retrieves the cached results from disk"
-        it "returns the cache object"
+        before do
+          @compiler.should_receive(:stale?).
+            and_return(false)
+        end
+
+        it "returns the cached results from disk" do
+          cache = mock('cache')
+          
+          Compiler::Cacheable::CacheDirectory.should_receive(:lookup).
+            and_return(cache)
+          
+          @compiler.cache(@procedure).should == cache
+        end
       end
     end
-
   end
 
   describe Compiler::Cacheable::CacheDirectory do
