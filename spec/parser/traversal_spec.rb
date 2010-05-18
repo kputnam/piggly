b2c25bf8b3e502454172b7027853d672a0ecfb11
kputnam <putnam.kyle@gmail.com>
2010-05-18 23:14:36 +0000

    Added spec outlines

---
+++ spec/parser/traversal_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+module Piggly
+
+  describe Parser::Nodes::Traversal do
+    describe "fold"
+    describe "reduce"
+    describe "count"
+    describe "find"
+    describe "select"
+    describe "flatten"
+  end
+
+end
