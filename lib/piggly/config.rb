f5a7d0015bdad4fecf5a4ac586e9299b236afcf0
kputnam <putnam.kyle@gmail.com>
2011-01-27 21:39:24 -0600

    Make Config instantiable (instead of singleton)

--- lib/piggly/config.rb
+++ lib/piggly/config.rb
@@ -1,39 +1,55 @@
 module Piggly
   class Config
-    class << self
 
-      def config_accessor(hash)
-        hash.keys.each do |name|
-          self.class.send(:define_method, name) do
-            instance_variable_get("@#{name}") || hash[name]
-          end
-          self.class.send(:define_method, "#{name}=") do |value|
-            instance_variable_set("@#{name}", value)
-          end
+    def self.instance
+      @instance ||= new
+    end
+
+    def self.method_missing(name, *args, &block)
+      if instance.respond_to?(name)
+        instance.send(name, *args, &block)
+      else
+        super
+      end
+    end
+
+    def self.respond_to?(name)
+      super or instance.respond_to?(name)
+    end
+
+    def self.config_accessor(hash)
+      hash = hash.clone
+
+      hash.keys.each do |name|
+        define_method(name) do
+          instance_variable_get("@#{name}") || hash[name]
+        end
+
+        define_method("#{name}=") do |value|
+          instance_variable_set("@#{name}", value)
         end
       end
+    end
 
-      def path(root, file=nil)
-        if file
-          file[%r{^\.\.|^\/|^(?:[A-Z]:)?/}i] ?
-            file : # ../path, /path, or D:\path that isn't relative to root
-            File.join(root, file)
-        else
-          root
-        end
+    def path(root, file=nil)
+      if file
+        file[%r{^\.\.|^\/|^(?:[A-Z]:)?/}i] ?
+          file : # ../path, /path, or D:\path that isn't relative to root
+          File.join(root, file)
+      else
+        root
       end
+    end
 
-      def mkpath(root, file=nil)
-        if file.nil?
-          FileUtils.makedirs(root)
-          root
-        else
-          path = path(root, file)
-          FileUtils.makedirs(File.dirname(path))
-          path
-        end
+    def mkpath(root, file=nil)
+      if file.nil?
+        FileUtils.makedirs(root)
+        root
+      else
+        path = path(root, file)
+        FileUtils.makedirs(File.dirname(path))
+        path
       end
-
     end
 
     config_accessor :cache_root   => File.expand_path(File.join(Dir.pwd, 'piggly', 'cache')),
