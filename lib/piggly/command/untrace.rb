bfbf7e880dae931cb4de0a7e950c45d6b9b8aa69
Kvle Putnam <putnam.kvle@gmail.com>
2011-08-10 23:12:45 -0500

    Set report path -o explicitly. Fix #9

--- lib/piggly/command/untrace.rb
+++ lib/piggly/command/untrace.rb
@@ -12,7 +12,7 @@ module Piggly
         procedures  = filter(config, index)
 
         if procedures.empty?
-          if filters.empty?
+          if config.filters.empty?
             abort "no stored procedures in the cache"
           else
             abort "no stored procedures in the cache matched your criteria"
