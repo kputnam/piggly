0787ce7385d09f5d33e05ff283070b457b652be7
Kvle Putnam <kputnam@users.noreply.github.com>
2016-10-29 21:39:31 -0500

    Rename test and add assertions

--- spec/028_spec.rb
+++ spec/028_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 module Piggly
-  describe "github issue #TBC" do
+  describe "github issue #28" do
     include GrammarHelper
 
       it "can parse the a GET DIAGNOSTICS expression" do
@@ -9,37 +9,38 @@ module Piggly
 
         node = parse(:statement, body)
         node.should be_statement
-        #node.count{|e| e.comment? }.should == 1
       end
 
       it "can parse a procedure with GET DIAGNOSTICS" do
         body = <<-SQL
-        declare
+        DECLARE
           text_var1 text;
           text_var2 text;
           text_var3 text;
-        begin
-          perform 1/0
-        exception when others
-          get stacked diagnostics text_var1 = MESSAGE_TEXT, text_var2 = PG_EXCEPTION_DETAIL, text_var3 = PG_EXCEPTION_HINT;
-        end
+        BEGIN
+          RETURN 1/0;
+        EXCEPTION WHEN SQLSTATE '22012' THEN
+          GET STACKED DIAGNOSTICS text_var1 = MESSAGE_TEXT,
+                                  text_var2 = PG_EXCEPTION_DETAIL,
+                                  text_var3 = PG_EXCEPTION_HINT;
+        END
         SQL
 
-        node = parse(:start,body.strip)
-
+        node = parse(:start, body.strip)
+        node.count{|e| e.branch? }.should == 1 # catch
+        node.find{|e| e.branch? }.body.source_text.strip.should =~ /^GET.+HINT;/m
       end
 
      it "can parse WITH <> AS <> SELECT <> SQL query" do
        body = <<-SQL
-        declare
-        begin
-            WITH n AS (SELECT first_name FROM users WHERE id > 1000)
+        DECLARE
+        BEGIN
+          WITH n AS (SELECT first_name FROM users where id > 1000)
             SELECT * FROM people WHERE people.first_name = n.first_name;
-
-	    RETURN 1;
-        end
+          RETURN 1;
+        END
        SQL
-puts "$$$#{body.strip.downcase}$$$"
+
        node = parse(:start, body.strip.downcase)
        node.count{|e| e.sql? }.should == 1
      end
