48fda0f565c8814e7a499716e078aadc8df0eaeb
kputnam <kputnam@users.noreply.github.com>
2017-11-01 18:19:07 -0500

    Bump version v2.2.4

--- lib/piggly/version.rb
+++ lib/piggly/version.rb
@@ -2,9 +2,9 @@ module Piggly
   module VERSION
     MAJOR = 2
     MINOR = 2
-    TINY  = 3
+    TINY  = 4
 
-    RELEASE_DATE = "2017-10-23"
+    RELEASE_DATE = "2017-11-01"
   end
 
   class << VERSION
