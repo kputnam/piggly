4bb25b0f48067cc4363e99f32218aba2a6313af9
Kvle Putnam <kputnam@users.noreply.github.com>
2018-04-18 21:14:11 -0500

    updated example report

--- reports/highlight.js
+++ reports/highlight.js
@@ -19,3 +19,20 @@ function unhighlight(id, bg) {
   var el = document.getElementById(id);
   if (el) { el.style.backgroundColor = bg; }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var toc = document.getElementsByClassName('toc');
+  var lis = document.getElementsByClassName('listing');
+  if (toc.length != 1 || lis.length != 1)
+    return;
+
+  window.onscroll = function() {
+    if (lis[0].getBoundingClientRect().y <= 0) {
+      toc[0].classList.add('toc-fixed');
+      lis[0].classList.add('listing-fixed');
+    } else {
+      toc[0].classList.remove('toc-fixed');
+      lis[0].classList.remove('listing-fixed');
+    }
+  }
+});
